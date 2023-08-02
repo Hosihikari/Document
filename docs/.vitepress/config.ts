b8a158eb0aeb9882ad900abc3a275842a1ac7a50
星梦 <starsdream00@icloud.com>
2023-08-02 12:21:36 +0800

    feat: Make it better

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,4 +1,38 @@
-export default {
-    title: "星光",
-    description: "BDS .NET plugin framework",
-};
+import { defineConfig } from 'vitepress'
+
+export default defineConfig({
+  lang: 'en-US',
+  title: '星光',
+  description: 'BDS .NET plugin framework',
+
+  lastUpdated: true,
+
+  sitemap: {
+    hostname: 'https://hshkr.top',
+    transformItems(items) {
+      return items.filter((item) => !item.url.includes('migration'))
+    }
+  },
+
+  themeConfig: {
+    siteTitle: '星光',
+
+    editLink: {
+      pattern: 'https://github.com/Hosihikari/Documentation/edit/main/docs/:path',
+      text: 'Edit this page on GitHub'
+    },
+
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/Hosihikari' }
+    ],
+
+    footer: {
+      message: 'Powered by Vitepress',
+      copyright: '© 2023 星光 GFDL-1.3'
+    },
+
+    search: {
+      provider: 'local'
+    }
+  }
+})
