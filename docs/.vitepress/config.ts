3f524d7c9dd8ff1444e738265c4c49eaafcc02ca
星梦 <starsdream00@icloud.com>
2023-08-05 03:52:59 +0800

    feat: Add zh_hans language

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,9 +1,7 @@
 import { defineConfig } from 'vitepress'
 
 export default defineConfig({
-  lang: 'en-US',
   title: '星光',
-  description: '.NET plugin framework for BDS',
 
   lastUpdated: true,
 
@@ -15,24 +13,52 @@ export default defineConfig({
   },
 
   themeConfig: {
-    siteTitle: '星光',
-
-    editLink: {
-      pattern: 'https://github.com/Hosihikari/Documentation/edit/main/docs/:path',
-      text: 'Edit this page on GitHub'
-    },
-
     socialLinks: [
-      { icon: 'github', link: 'https://github.com/Hosihikari' }
+      {
+        icon: 'github',
+        link: 'https://github.com/Hosihikari'
+      }
     ],
-
-    footer: {
-      message: 'Powered by Vitepress',
-      copyright: '© 2023 星光 GFDL-1.3'
-    },
 
     search: {
       provider: 'local'
+    }
+  },
+
+  locales: {
+    root: {
+      label: 'English',
+      lang: 'en-US',
+      description: '.NET plugin framework for BDS',
+
+      themeConfig: {
+        editLink: {
+          pattern: 'https://github.com/Hosihikari/Documentation/edit/main/docs/:path',
+          text: 'Edit this page on GitHub'
+        },
+
+        footer: {
+          message: 'Powered by Vitepress',
+          copyright: '© 2023 星光 GFDL-1.3'
+        }
+      }
+    },
+    zh_hans: {
+      label: '简体中文',
+      lang: 'zh-Hans',
+      description: 'BDS .NET插件框架',
+
+      themeConfig: {
+        editLink: {
+          pattern: 'https://github.com/Hosihikari/Documentation/edit/main/docs/:path',
+          text: '在GitHub上编辑此页面'
+        },
+
+        footer: {
+          message: '由Vitepress提供支持',
+          copyright: '© 2023 星光 GNU自由文档许可证v1.3'
+        }
+      }
     }
   }
 })
---
+++ docs/zh_hans/index.md
@@ -0,0 +1,29 @@
+---
+layout: home
+
+title: 星光
+titleTemplate: BDS .NET插件框架
+
+hero:
+  name: 星光
+  text: 用.NET让BDS更强大
+  tagline: 轻量且灵活。见见你一直想要的BDS .NET插件框架。
+  actions:
+    - theme: brand
+      text: 开始使用
+      link: /zh_hans/guide/getting-started
+    - theme: alt
+      text: 开发手册
+      link: /zh_hans/develop/getting-started
+
+features:
+  - icon: 💪
+    title: 强大的托管运行时
+    details: 使用最新的.NET技术拓展你的BDS。
+  - icon: ✨
+    title: 高度模块化
+    details: 模块化设计，让BDS与插件之间更加兼容。
+  - icon: ☂️
+    title: 保证数据完整性
+    details: 安全的API，让插件更加可控。
+---
